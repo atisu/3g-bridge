619b3e5990c40296ca6c694ac010214cb7e33da1
kornaf <kornaf@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-02-21 11:22:32 +0000

    modified protorun script
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1379 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- protorun.sh
+++ protorun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 JOBID=10374399
-INPUTPATH=/var/lib/boinc/szdgr/cg_master
+INPUTPATH=/var/lib/boinc/cancergrid/master
 
 # Step 1
 # DB read
@@ -10,22 +10,24 @@ INPUTPATH=/var/lib/boinc/szdgr/cg_master
 # To be executed locally
 
 # Step 3
-OUTPUT=`./injector -j "2D3D Converter Job 1" -a 2d3dconv -c "-p params.2D3D $JOBID.2D.mol" -i params.2D3D,$JOBID.2D.mol -p $INPUTPATH/params.2D3D,$INPUTPATH/$JOBID.2D.mol -o $JOBID.3D.mol`
-echo "Output is $OUTPUT"
+CMOL3D=`./injector -j "2D3D Conv Job 1" -a cmol3d -c "-p params.2D3D $JOBID.2D.mol" -i params.2D3D,$JOBID.2D.mol -p $INPUTPATH/genmaster_2d3d/inputs/params.2D3D,$INPUTPATH/genmaster_2d3d/inputs/$JOBID.2D.mol -o $JOBID.3D.mol`
+echo "Output of 2D3D Converter is $CMOL3D"
 
 # Step 4
-OUTPUT=`./injector -j "Conformer Generator Job 1" -a flexmol -c "-p params.conf $JOBID.mol" -i params.conf,$JOBID.3D.mol -p $INPUTPATH/params.conf,$OUTPUT -o $JOBID.conf.sd`
-echo "Output is $OUTPUT"
+CONFGEN=`./injector -j "Conf Gen Job 1" -a cmol3d -c "-p params.conf $JOBID.mol" -i params.conf,$JOBID.3D.mol -p $INPUTPATH/genmaster_confgen/inputs/params.conf,$CMOL3D -o $JOBID.conf.sd`
+echo "Output of Conformer Generator is $CONFGEN"
 
 # Step 5
 # To be executed locally
+echo "Skipping SD->MOL Converter"
 
 # Step 6
-OUTPUT=`./injector -j "Mopac Job 1" -a mopac -c "-p params.mopac $JOBID.1" -i params.mopac,$JOBID.1.mol -p $INPUTPATH/params.mopac,$INPUTPATH/$JOBID.1.mol -o $JOBID.1.mop`
-echo "Output is $OUTPUT"
+MOPAC=`./injector -j "Mopac Job 1" -a mopac -c "-p params.mopac $JOBID.1" -i params.mopac,$JOBID.1.mol -p $INPUTPATH/genmaster_mopac/inputs/params.mopac,$INPUTPATH/genmaster_mopac/inputs/$JOBID.1.mol -o $JOBID.1.mop`
+echo "Output of Mopac is $MOPAC"
 
-# Step 7 
-OUTPUT=`./injector -k "Molecule Descriptor Calculator Job 1" -a moldesccalc -c "-p params.mdc $JOBID.1" -i params.mdc,$JOBID.1 -p $INPUTPATH/params.mdc,$INPUTPATH/$JOBID.1 -o $JOBID.1.desc`
-echo "Output is $OUTPUT"
+# Step 7
+MOLDESCCALC=`./injector -k "Mol Desc Calc Job 1" -a moldesccalc -c "-p params.mdc $JOBID.1" -i params.mdc,$JOBID.1.mol,$JOBID.1.mop -p $INPUTPATH/genmaster_moldesccalc/inputs/params.mdc,$INPUTPATH/genmaster_moldesccalc/inputs/$JOBID.1.mol,$MOPAC -o $JOBID.1.desc`
+echo "Output of Molecule Descriptor Calculator is $MOLDESCCALC"
 
 echo "End of prototype run"
+
