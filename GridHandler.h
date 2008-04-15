b74f835650ef37ca37045c0b7fd743bff553cba9
zfarkas <zfarkas@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-04-15 11:49:15 +0000

    Added GridHandler interface.
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1544 a7169a2c-3604-0410-bc95-c702d8d87f7a

---
+++ GridHandler.h
@@ -0,0 +1,38 @@
+#ifndef __GRIDHANDLER_H
+#define __GRIDHANDLER_H
+
+#include <vector>
+#include "CGJob.h"
+
+using namespace std;
+
+/*
+ * GridHandler interface. Used by the Queue Manager to:
+ *  - submit jobs (as WUs)
+ *  - query status of jobs (or WUs)
+ *  - cancel jobs (or WUs)
+ *  - get output
+ */
+class GridHandler {
+public:
+    /**
+     * Initialize GridHandler. For DC-API this should call DC_initMaster, for
+     * EGEE should create the config context, ...
+     */
+    virtual GridHandler() = 0;
+    /**
+     * Submit jobs in the argument. Set the different properties of jobs (Grid
+     * ID, status, ...) through the objects' property change functions.
+     */
+    virtual void submitJobs(vector<CGJob *> *jobs) = 0;
+    /**
+     * Update status of jobs through the objects.
+     */
+    virtual void getStatus(vector<CGJob *> *jobs) = 0;
+    /**
+     * Get results.
+     */
+    virtual void getOutputs(vector<CGJob *> *jobs) = 0;
+};
+
+#endif
