8a584a6e8c6bd6138c0d95716e1e77a02b91c6db
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-04-30 12:39:46 +0000

    Generate config.h and include it from every source file
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1562 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- CGJob.cpp
+++ CGJob.cpp
@@ -1,3 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <map>
 #include <string>
 #include <cstring>
--- EGEEHandler.cpp
+++ EGEEHandler.cpp
@@ -1,3 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "CGJob.h"
 #include "GridHandler.h"
 #include "EGEEHandler.h"
