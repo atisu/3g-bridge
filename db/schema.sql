067baeff24eaa8c1e701a20c25797737e8d14097
zfarkas <zfarkas@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-05-07 12:03:45 +0000

    Short descriptions, schema updates
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1589 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- db/schema.sql
+++ db/schema.sql
@@ -5,27 +5,41 @@ drop table if exists cg_job;
 drop table if exists cg_inputs;
 drop table if exists cg_outputs;
 
+
+/*
+ * Job table
+ */
 create table cg_job (
-    id			varchar(254) not null,
-    args		varchar(254) not null,
-    alg			varchar(254) not null,
-    status		varchar(254) not null,
-    wuid		varchar(254) not null,
-    property		varchar(254) not null,
-    creation_time	timestamp    null default current_timestamp,
+    id			varchar(254) not null /* Job identifier */,
+    args		varchar(254) not null /* Arguments */,
+    alg			varchar(254) not null /* Algorithm (executable) name */,
+    status		varchar(254) not null /* Job's status */,
+    gridid		varchar(254) not null /* Job's grid identifier */,
+    property		varchar(254) not null /* Property? */,
+    dsttype		varchar(254)	      /* Destination type */,
+    dstloc		varchar(254)	      /* Destination location */,
+    creation_time	timestamp    null default current_timestamp /* Creation time */,
     primary key (id)
 ) type=InnoDB;
 
+
+/*
+ * Job input table
+ */
 create table cg_inputs (
-    id                  varchar(254) not null,
-    localname           varchar(254) not null,
-    path		varchar(254) not null,
+    id                  varchar(254) not null /* Job's identifier */,
+    localname           varchar(254) not null /* Basename of the file */,
+    path		varchar(254) not null /* Location of the file */,
     primary key (id)
 ) type=InnoDB;
 
+
+/*
+ * Job output table
+ */
 create table cg_outputs (
-    id                  varchar(254) not null,
-    localname           varchar(254) not null,
-    path		varchar(254),
+    id                  varchar(254) not null /* Job's identifier */,
+    localname           varchar(254) not null /* Basename of the file */,
+    path		varchar(254)          /* Expected location of the file */,
     primary key (id)
 ) type=InnoDB;
