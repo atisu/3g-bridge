3b99632a0a11e8dfb568a1eb53cbd2b275bb4e4d
zfarkas <zfarkas@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-05-07 10:46:10 +0000

    Updated schema file.
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1586 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,31 @@
 /*
  * Schema description for CG
  */
-drop table cg_job;
-drop table cg_inputs;
-drop table cg_outputs;
+drop table if exists cg_job;
+drop table if exists cg_inputs;
+drop table if exists cg_outputs;
 
 create table cg_job (
-    id                  integer     not null auto_increment,
-    name                varchar(254) not null,
+    id			varchar(254) not null,
     args		varchar(254) not null,
-    algname		varchar(254) not null,
+    alg			varchar(254) not null,
     status		varchar(254) not null,
     wuid		varchar(254) not null,
+    property		varchar(254) not null,
+    creation_time	timestamp    null default current_timestamp,
     primary key (id)
 ) type=InnoDB;
 
 create table cg_inputs (
-    id                  integer     not null auto_increment,
+    id                  varchar(254) not null,
     localname           varchar(254) not null,
     path		varchar(254) not null,
-    jobid		integer,
     primary key (id)
 ) type=InnoDB;
 
 create table cg_outputs (
-    id                  integer     not null auto_increment,
+    id                  varchar(254) not null,
     localname           varchar(254) not null,
     path		varchar(254),
-    jobid		integer,
     primary key (id)
 ) type=InnoDB;
