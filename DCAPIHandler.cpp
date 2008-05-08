2d85df0f3fb9d8ff95cc4cf2fd134a2cf3c7c916
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-05-08 17:00:20 +0000

    Batch handling updates
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1607 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- DCAPIHandler.cpp
+++ DCAPIHandler.cpp
@@ -13,15 +13,17 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 
 #define SCRIPT_NAME		"script"
-#define INPUTS_DIR		"inputs"
-#define INPUTS_NAME		"inputs.tar.gz"
-#define OUTPUTS_DIR		"outputs"
-#define OUTPUTS_NAME		"outputs.tar.gz"
-#define OUTPUTS_PATTERN		"outputs/*"
+#define INPUT_DIR		"inputs"
+#define INPUT_NAME		"inputs.tar.gz"
+#define OUTPUT_DIR		"outputs"
+#define OUTPUT_NAME		"outputs.tar.gz"
+#define OUTPUT_PATTERN		"outputs/*"
 
 using namespace std;
 
@@ -49,6 +51,34 @@ static string substitute(const string src, const string key, string value)
 	}
 }
 
+static void invoke_cmd(const char *exe, const char *const argv[]) throw (BackendException &)
+{
+	pid_t pid = fork();
+	if (pid)
+	{
+		/* Parent */
+		int status;
+
+		if (waitpid(pid, &status, 0) == -1)
+			throw new BackendException(string("waitpid() failed: ") + strerror(errno));
+		if (!WIFEXITED(status))
+			throw new BackendException(string("Command ") + exe + " died");
+		if (WEXITSTATUS(status))
+			throw new BackendException(string("Command ") + exe + " exited with non-zero status");
+		return;
+	}
+
+	/* Child */
+	int fd = open("/dev/null", O_RDWR);
+	dup2(fd, 0);
+	dup2(fd, 1);
+	dup2(fd, 2);
+	close(fd);
+
+	execvp(exe, (char *const *)argv);
+	_Exit(255);
+}
+
 static string get_dc_client_config(const string &app, const char *key, bool strict = false)
 {
 	char *value = DC_getClientCfgStr(app.c_str(), key, 0);
@@ -72,24 +102,21 @@ DCAPIHandler::~DCAPIHandler()
 {
 }
 
-static string setup_workdir(void)
+static string setup_workdir(void) throw (BackendException &)
 {
 	char buf[PATH_MAX];
 
 	snprintf(buf, sizeof(buf), "batch_XXXXXX");
 	if (!mkdtemp(buf))
-	{
-		/* XXX Log */
-		throw errno;
-	}
+		throw new BackendException(string("Failed to create directory: ") + strerror(errno));
 
 	return string(buf);
 }
 
 static void emit_job(CGJob *job, const string &basedir, ofstream &script, const string &job_template) throw (BackendException &)
 {
-	string input_dir = INPUTS_DIR "/" + job->getId();
-	string output_dir = OUTPUTS_DIR "/" + job->getId();
+	string input_dir = INPUT_DIR "/" + job->getId();
+	string output_dir = OUTPUT_DIR "/" + job->getId();
 
 	string input_path = basedir + "/" + input_dir;
 	if (mkdir(input_path.c_str(), 0750))
@@ -110,7 +137,11 @@ static void emit_job(CGJob *job, const string &basedir, ofstream &script, const 
 
 		/* First, try to create a hard link; if that fails, do a copy */
 		if (link(src.c_str(), dst.c_str()))
-			copy_file(src, dst);
+		{
+			const char *cp_args[] =
+				{ "cp", "-p", src.c_str(), dst.c_str(), 0 };
+			invoke_cmd("cp", cp_args);
+		}
 	}
 
 	string tmpl = substitute(job_template, "input_dir", input_dir);
@@ -120,41 +151,11 @@ static void emit_job(CGJob *job, const string &basedir, ofstream &script, const 
 	script << tmpl;
 }
 
-static void submit_internal(vector<CGJob *> *jobs, const string &basedir, ofstream &script, const string &job_template) throw (BackendException &)
-{
-	snprintf(input_name, sizeof(input_name), "inputs_XXXXXX");
-	int fd = mkstemp(input_name);
-	if (fd == -1)
-		throw new BackendException(string("Failed to create input file: ") + strerror(errno));
-	close(fd);
-
-	string cmd("tar -C '" + basedir + "' -c -z -f '" + input_name + " " INPUTS_DIR + " " + OUTPUTS_DIR);
-	system(cmd);
-
-	const char *args[1];
-	args[0] = SCRIPT_NAME;
-	args[1] = NULL;
-	wu = DC_createWU(algname.c_str(), args, 0, NULL);
-	if (!wu)
-		throw new BackendException("Out of memory");
-
-	DC_addWUInput(wu, INPUTS_NAME, input_name);
-	DC_addWUOutput(wu, OUTPUTS_NAME);
-
-	DC_submitWU(wu);
-
-	char *wu_id = DC_getWUId(wu);
-	for (vector<CGJob *>::const_iterator it = jobs->begin(); it != jobs->end(); it++)
-	{
-		job->setGridId(wu_id);
-		job->setStatus(CG_RUNNING);
-	}
-	free(wu_id);
-}
-
 void DCAPIHandler::submitJobs(vector<CGJob *> *jobs) throw (BackendException &)
 {
+	char input_name[PATH_MAX] = { 0 };
 	BackendException *err;
+	DC_Workunit *wu;
 	string basedir;
 	int ret;
 
@@ -171,31 +172,68 @@ void DCAPIHandler::submitJobs(vector<CGJob *> *jobs) throw (BackendException &)
 	string job_template = load_file(get_dc_client_config(algname, "BatchJobTemplate", true));
 	string tail_template = load_file(get_dc_client_config(algname, "BatchTailTemplate", true));
 
-	basedir = setup_workdir();
-	ofstream script((basedir + "/" SCRIPT_NAME).c_str(), ios::out);
-	script << substitute(head_template, "inputs", INPUTS_NAME);
-
 	try
 	{
+		basedir = setup_workdir();
+		string script_name = basedir + "/" SCRIPT_NAME;
+		ofstream script(script_name.c_str(), ios::out);
+		script << substitute(head_template, "inputs", INPUT_NAME);
+
 		double total = jobs->size();
-		for (vector<CGJob *>::const_iterator it = jobs->begin(), int i = 0; it != jobs->end(); it++, i++)
+		int cnt = 0;
+		for (vector<CGJob *>::const_iterator it = jobs->begin(); it != jobs->end(); it++, cnt++)
 		{
 			char cmd[128];
 
 			emit_job(*it, basedir, script, job_template);
-			snprintf(cmd, sizeof(cmd), "boinc fraction_done %.6g\n", total / i);
+			snprintf(cmd, sizeof(cmd), "boinc fraction_done %.6g\n", total / cnt);
 			script << cmd;
 		}
 
-		submit_internal(jobs, basedir, script, job_template);
+		snprintf(input_name, sizeof(input_name), "inputs_XXXXXX");
+		int fd = mkstemp(input_name);
+		if (fd == -1)
+			throw new BackendException(string("Failed to create file: ") + strerror(errno));
+		close(fd);
 
-		tail_template = substitute(tail_template, "outputs", OUTPUTS_NAME);
+		const char *tarargs[] =
+		{
+			"tar", "-C", basedir.c_str(), "-c", "-z", "-f", input_name, INPUT_DIR, OUTPUT_DIR, 0
+		};
+		invoke_cmd("tar", (char *const *)tarargs);
+
+		const char *wu_args[] = { SCRIPT_NAME, 0 };
+		wu = DC_createWU(algname.c_str(), wu_args, 0, NULL);
+		if (!wu)
+			throw new BackendException("Out of memory");
+
+		DC_addWUInput(wu, INPUT_NAME, input_name, DC_FILE_VOLATILE);
+		DC_addWUInput(wu, SCRIPT_NAME, script_name.c_str(), DC_FILE_VOLATILE);
+		DC_addWUOutput(wu, OUTPUT_NAME);
+
+		if (DC_submitWU(wu))
+			throw new BackendException("WU submission failed");
+
+		char *wu_id = DC_getWUId(wu);
+		for (vector<CGJob *>::const_iterator it = jobs->begin(); it != jobs->end(); it++)
+		{
+			(*it)->setGridId(wu_id);
+			(*it)->setStatus(CG_RUNNING);
+		}
+		free(wu_id);
+
+		tail_template = substitute(tail_template, "outputs", OUTPUT_NAME);
 		tail_template = substitute(tail_template, "output_pattern", OUTPUT_PATTERN);
 		script << tail_template;
 	}
 	catch (BackendException &e)
 	{
-		err = e;
+		err = &e;
+
+		if (input_name[0])
+			unlink(input_name);
+		if (wu)
+			DC_destroyWU(wu);
 	}
 
 	/* Remove the temp. directory */
@@ -211,6 +249,7 @@ void DCAPIHandler::submitJobs(vector<CGJob *> *jobs) throw (BackendException &)
 
 void DCAPIHandler::updateStatus(void) throw (BackendException &)
 {
+#if 0
 	DC_MasterEvent *event;
 	char *wutag;
 	uuid_t *id = new uuid_t[1];
@@ -249,4 +288,5 @@ void DCAPIHandler::updateStatus(void) throw (BackendException &)
 			}
 		}
 	}
+#endif
 }
