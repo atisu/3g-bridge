8d6a57961fd7c5fd79e49635aa67f74a6e480459
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-05-21 20:54:33 +0000

    Oops, forgot to add a file
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1647 a7169a2c-3604-0410-bc95-c702d8d87f7a

---
+++ Logging.cpp
@@ -0,0 +1,57 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "Logging.h"
+
+#include <time.h>
+
+static ostream *log_stream;
+static int log_level;
+static const char *level_str[] =
+{
+	"EMERG", "ALERT", "CRIT", "ERROR", "WARNING", "NOTICE", "INFO", "DEBUG"
+};
+
+static void print_header(int level)
+{
+	time_t now = time(NULL);
+	struct tm *tm = localtime(&now);
+	char buf[37];
+	strftime(buf, sizeof(buf), "%F %T", tm);
+
+	const char *str = "UNKNOWN";
+	if (level >= 0 && level < sizeof(level_str) / sizeof(level_str[0]))
+		str = level_str[level];
+
+	*log_stream << buf << ' ' << str << ": ";
+}
+
+void Logging::init(ostream &stream, int level)
+{
+	log_stream = &stream;
+	log_level = level;
+}
+
+void Logging::log(int lvl, const char *fmt, va_list ap) {
+	if (lvl > log_level)
+		return;
+
+	print_header(lvl);
+	char *str;
+	vasprintf(&str, fmt, ap);
+	*log_stream << str << endl;
+	free(str);
+}
+
+void Logging::log(int lvl, const char *fmt, ...) {
+	va_list ap;
+
+	va_start(ap, fmt);
+	Logging::log(lvl, fmt, ap);
+	va_end(ap);
+}
+
+void Logging::log(int lvl, const string &str) {
+	Logging::log(lvl, str.c_str());
+}
