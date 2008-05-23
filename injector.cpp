8b8c1ffec22b14239894defed7cd2d6845e23153
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-05-23 09:13:48 +0000

    Rework command-line handling Output file paths must be specified by the injector as well Improve config error handling
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1669 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- injector.cpp
+++ injector.cpp
@@ -39,14 +39,13 @@ void usage(const char *cmdname)
 {
     cout << "--== Prototype job submitter for Cancergrid ==--" << endl << endl;
     cout << "Usage: " << cmdname << " config [switches]" << endl;
-    cout << " * -a --algname  [algname]  algorithm (executable) name" << endl;
-    cout << "   -c --cmdline  [cmdline]  command line options" << endl;
-    cout << " * -i --inlocal  [inlocal]  comma separated list of input files used" << endl;
-    cout << " * -p --inpath   [inpath]   comma separated list of input paths used" << endl;
-    cout << "   -o --outlocal [outlocal] comma separated list of output files used" << endl;
-    cout << "   -t --type     [type]     destination type (DCAPI, EGEE)" << endl;
-    cout << "   -w --wait                use if the tool should wait for the job to finish" << endl;
-    cout << "   -h --help                this help screen" << endl;
+    cout << " * -a, --algname <NAME>          algorithm (executable) name" << endl;
+    cout << "   -c, --cmdline <ARGUMENTS>     command-line arguments" << endl;
+    cout << " * -i, --input <LOGICAL>:<PATH>  logical/physical input file name" << endl;
+    cout << " * -o, --output <LOGICAL>:<PATH> logical/physical output file name" << endl;
+    cout << "   -t, --type <DEST>             destination type (DCAPI, EGEE)" << endl;
+    cout << "   -w, --wait                    use if the tool should wait for the job to finish" << endl;
+    cout << "   -h, --help                    this help screen" << endl;
     cout << " *: switch is mandatory" << endl;
     exit(0);
 }
@@ -56,9 +55,8 @@ int main(int argc, char **argv)
     Connection con;
     string cmdLine = "";
     string algName = "";
-    string inLocal = "";
-    string inPath = "";
-    string outLocal = "";
+    vector<string *> inputs;
+    vector<string *> outputs;
     string type = "";
     bool wait = false;
 
@@ -68,9 +66,8 @@ int main(int argc, char **argv)
 	static struct option long_options[] = {
 	    {"algname", 1, 0, 'a'},
 	    {"cmdline", 1, 0, 'c'},
-	    {"inlocal", 1, 0, 'i'},
-	    {"inpath", 1, 0, 'p'},
-	    {"outlocal", 1, 0, 'o'},
+	    {"input", 1, 0, 'i'},
+	    {"output", 1, 0, 'o'},
 	    {"type", 1, 0, 't'},
 	    {"wait", 1, 0, 'w'},
 	    {"help", 0, 0, 'h'},
@@ -87,13 +84,10 @@ int main(int argc, char **argv)
 		algName = string(optarg);
 		break;
 	    case 'i':
-		inLocal = string(optarg);
-		break;
-	    case 'p':
-		inPath = string(optarg);
+		inputs.push_back(new string(optarg));
 		break;
 	    case 'o':
-		outLocal = string(optarg);
+		outputs.push_back(new string(optarg));
 		break;
 	    case 't':
 		type = string(optarg);
@@ -108,14 +102,30 @@ int main(int argc, char **argv)
     }
 
     Logging::init(cout, LOG_DEBUG);
-    QMConfig config(argv[optind]);
-    string dbname = config.getStr("DB_NAME");
-    string host = config.getStr("DB_HOST");
-    string user = config.getStr("DB_USER");
-    string passwd = config.getStr("DB_PASSWORD");
+
+    if (optind >= argc)
+    {
+	cerr << "Configuration file must be specified" << endl;
+	exit(1);
+    }
+
+    string dbname, host, user, passwd;
+
+    try {
+	QMConfig config(argv[optind]);
+
+	dbname = config.getStr("DB_NAME");
+	host = config.getStr("DB_HOST");
+	user = config.getStr("DB_USER");
+	passwd = config.getStr("DB_PASSWORD");
+    }
+    catch (string &s) {
+        cerr << "Error: " << s << endl;
+	exit(1);
+    }
 
     // Check for mandatory options
-    if (algName == "" || inLocal == "" || inPath == "")
+    if (algName == "" || !inputs.size() || !outputs.size())
 	usage(argv[0]);
 
     // If no type is specified, use DCAPI
@@ -132,40 +142,42 @@ int main(int argc, char **argv)
         con.connect(dbname.c_str(), host.c_str(), user.c_str(), passwd.c_str());
 	Query query = con.query();
 	
-	// explode list into vector<string>
-	vector<string> in = explode(',', inLocal);
-	vector<string> inp = explode(',', inPath);
-	vector<string> out = explode(',', outLocal);
-	map<string, string> *inputs = new map<string, string>();
-	
-	if (in.size() != inp.size()) {
-	    cerr << "Input local names and input path parameters must have the same number of elements" << endl;
-	    exit(-1);
-	}
-	
-	// make map of input local names and input paths
-	for (unsigned i = 0; i < in.size(); i++)
-	    inputs->insert(make_pair(in.at(i), inp.at(i)));
-	    
 //	{ // transaction scope
 //	    Transaction trans(con);
     	    // insert into mysql...
 	    query << "INSERT INTO cg_job(id, alg, status, args) VALUES(\"" << sid << "\",\"" << algName << "\",\"INIT\",\"" << cmdLine << "\")";
 	    query.execute();
 
-	    // Put inputs in cg_inputs table
-	    for(map<string, string>::iterator it = inputs->begin(); it != inputs->end(); it++) {
+	    for (vector<string *>::const_iterator it = inputs.begin(); it != inputs.end(); it++)
+	    {
+		size_t pos = (*it)->find_first_of(':');
+		if (pos == string::npos)
+		{
+		    cerr << "Invalid input specification: " << *it << endl;
+		    exit(1);
+		}
+		string logical = (*it)->substr(0, pos);
+		string path = (*it)->substr(pos + 1);
 		query.reset();
-		query << "INSERT INTO cg_inputs VALUES(\"" << sid << "\",\"" << it->first << "\",\"" << it->second << "\");";
+		query << "INSERT INTO cg_inputs (id, localname, path) VALUES(\"" << sid << "\",\"" << logical << "\",\"" << path << "\");";
 		query.execute();
 	    }
 
-	    // Put outputs in cg_outputs table
-	    for(vector<string>::iterator it = out.begin(); it != out.end(); it++) {
+	    for (vector<string *>::const_iterator it = outputs.begin(); it != outputs.end(); it++)
+	    {
+		size_t pos = (*it)->find_first_of(':');
+		if (pos == string::npos)
+		{
+		    cerr << "Invalid output specification: " << *it << endl;
+		    exit(1);
+		}
+		string logical = (*it)->substr(0, pos);
+		string path = (*it)->substr(pos + 1);
 		query.reset();
-		query << "INSERT INTO cg_outputs(id,localname) VALUES(\"" << sid << "\",\"" << *it << "\");";
+		query << "INSERT INTO cg_outputs (id, localname, path) VALUES(\"" << sid << "\",\"" << logical << "\",\"" << path << "\");";
 		query.execute();
 	    }
+
 //	    trans.commit();
 //	} // end of transaction scope
 
