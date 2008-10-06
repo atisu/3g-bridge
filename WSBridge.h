ff2a3e774713c4108fc3c68ad550b1266f05f072
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-10-06 16:09:24 +0000

    Improved web service definition header. Does not build yet.
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1859 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- WSBridge.h
+++ WSBridge.h
@@ -1,26 +1,39 @@
 //gsoap ns service name: gggbridge
 //gsoap ns service style: rpc
-//gsoap ns service encoding: encoded
+//gsoap ns service encoding: literal
 //gsoap ns service port: http://localhost:8080
 //gsoap ns service namespace: urn:gggbridge
+#import "stl.h"
 
-struct fnameArray
+struct ns__LogicalFile
 {
-	std::string *__ptr;
-	int __size;
-	int __offset;
+	std::string logicalName;
+	std::string URL;
 };
 
 struct ns__Job
 {
+	// Name of the already registered algorithm
 	std::string alg;
+	// Name of the grid plugin instance to submit to
 	std::string grid;
-	std::string args;
-	struct fnameArray *inputs;
-	struct fnameArray *outputs;
+	// List of command-line arguments
+	std::vector<std::string> *args;
+	// List of input files
+	std::vector<struct ns__LogicalFile> *inputs;
+	// List of output file names
+	std::vector<std::string> *output;
 };
 
-int ns__addJob(struct ns__Job job, std::string &jobid);
-int ns__getStatus(std::string jobid, std::string &status);
-int ns__cancel(std::string jobid, struct ns__cancelResponse {} *out);
-int ns__getOutput(std::string jobid, struct ns__getOutputResponse {} *out);
+enum ns__JobStatus { UNKNOWN, INIT, RUNNING, FINISHED, ERROR, CANCEL };
+
+struct ns__JobOutput
+{
+	std::string jobid;
+	std::vector<struct ns__LogicalFile> *outputs;
+};
+
+int ns__addJob(std::vector<struct ns__Job> *jobs, std::vector<std::string> *jobid);
+int ns__getStatus(std::vector<std::string> *jobids, std::vector<enum ns__jobStatus> *statuses);
+int ns__cancel(std::vector<std::string> *jobids, struct ns__cancelResponse {} *out);
+int ns__getOutput(std::vector<std::string> *jobids, std::vector<struct ns__JobOutput> *out);
