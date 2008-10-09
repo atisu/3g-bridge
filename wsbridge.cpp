82f0d1eee7da33d18d2fb7a978ffa01183d2db09
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-10-09 15:06:16 +0000

    First version of the web service that at least compiles
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1869 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- wsbridge.cpp
+++ wsbridge.cpp
@@ -1,27 +1,219 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "Conf.h"
+#include "DBHandler.h"
+#include "Job.h"
+#include "Logging.h"
+
 #include <string>
+
 #include <stdlib.h>
-#include <pthread.h>
+#include <signal.h>
 #include <uuid/uuid.h>
 
-#include "Job.h"
-#include "DBHandler.h"
 #include "soap/soapH.h"
-#include "soap/gggbridge.nsmap"
+#include "soap/G3BridgeBinding.nsmap"
 
+#include <glib.h>
 
 using namespace std;
 
+/**********************************************************************
+ * Global variables
+ */
 
-static char *workdir;
-void *process_request(void *soap);
+static char *download_dir;
+static char *partial_dir;
+static char *output_dir;
+static char *output_url_prefix;
+
+static volatile int finish;
+
 GKeyFile *global_config = NULL;
 
+static GThreadPool *dl_pool;
+static GThreadPool *soap_pool;
+
+/**********************************************************************
+ * The download handler function
+ */
+
+static void download_handler(void *data, void *user_data G_GNUC_UNUSED)
+{
+	/* XXX */
+}
+
+/**********************************************************************
+ * Schedule the download of an input file
+ */
+
+static void start_download(const string jobid, const string localName,
+		const string URL, const string path)
+{
+	/* XXX */
+}
+
+/**********************************************************************
+ * Calculate the file system location where an input file should be downloaded to
+ */
+
+static void calc_input_path(const string jobid, const string localName, string &path)
+{
+	/* XXX */
+}
+
+/**********************************************************************
+ * Calculate the location where an output file will be stored
+ */
+
+static void calc_output_path(const string jobid, const string localName, string &path)
+{
+	/* XXX */
+}
+
+/**********************************************************************
+ * Web service routines
+ */
+
+int G3BridgeOp__submit(struct soap*, G3BridgeType__JobList *jobs, struct G3BridgeOp__submitResponse &jobids)
+{
+	jobids.jobids = new G3BridgeType__JobIDList;
+	for (vector<G3BridgeType__Job *>::const_iterator jobit = jobs->job.begin(); jobit != jobs->job.end(); jobit++)
+	{
+		uuid_t uuid;
+		char jobid[37];
+
+        	uuid_generate(uuid);
+	        uuid_unparse(uuid, jobid);
+
+		G3BridgeType__Job *wsjob = *jobit;
+        	Job qmjob((const char *)jobid, wsjob->alg.c_str(), wsjob->grid.c_str(), wsjob->args.c_str(), Job::PREPARE);
+
+		for (vector<G3BridgeType__LogicalFile *>::const_iterator inpit = wsjob->inputs.begin(); inpit != wsjob->inputs.end(); inpit++)
+		{
+			G3BridgeType__LogicalFile *lfn = *inpit;
+
+			string path;
+			calc_input_path(jobid, lfn->logicalName, path);
+			qmjob.addInput(lfn->logicalName, path);
+
+			start_download(jobid, lfn->logicalName, lfn->URL, path);
+		}
+
+		for (vector<string>::const_iterator outit = wsjob->outputs.begin(); outit != wsjob->outputs.end(); outit++)
+		{
+			string path;
+
+			calc_output_path(jobid, *outit, path);
+			qmjob.addOutput(*outit, path);
+		}
+
+		DBHandler *dbh = DBHandler::get();
+		dbh->addJob(qmjob);
+		DBHandler::put(dbh);
+	}
+
+	return SOAP_OK;
+}
+
+
+int G3BridgeOp__getStatus(struct soap*, G3BridgeType__JobIDList *jobids, struct G3BridgeOp__getStatusResponse &statuses)
+{
+
+	statuses.statuses = new G3BridgeType__StatusList();
+
+        DBHandler *dbh = DBHandler::get();
+
+	for (vector<string>::const_iterator it = jobids->jobid.begin(); it != jobids->jobid.end(); it++)
+	{
+		G3BridgeType__JobStatus status = G3BridgeType__JobStatus__UNKNOWN;
+		Job job;
+
+		try
+		{
+			dbh->getJob(job, *it);
+			switch (job.getStatus())
+			{
+				case Job::PREPARE:
+				case Job::INIT:
+					status = G3BridgeType__JobStatus__INIT;
+					break;
+				case Job::RUNNING:
+					status = G3BridgeType__JobStatus__RUNNING;
+					break;
+				case Job::FINISHED:
+					status = G3BridgeType__JobStatus__FINISHED;
+					break;
+				case Job::ERROR:
+				case Job::CANCEL:
+					status = G3BridgeType__JobStatus__ERROR;
+					break;
+			}
+		}
+		catch (...)
+		{
+		}
+
+		statuses.statuses->status.push_back(status);
+	}
+
+	DBHandler::put(dbh);
+	return SOAP_OK;
+}
+
+
+int G3BridgeOp__delJob(struct soap*, G3BridgeType__JobIDList *jobids, struct G3BridgeOp__delJobResponse &_param_3 G_GNUC_UNUSED)
+{
+        DBHandler *dbh = DBHandler::get();
+	for (vector<string>::const_iterator it = jobids->jobid.begin(); it != jobids->jobid.end(); it++)
+	{
+		/* XXX Abort all pending transfers */
+		/* XXX Delete input/output files */
+		dbh->deleteJob(*it);
+	}
+	DBHandler::put(dbh);
+	return SOAP_OK;
+}
+
+
+int G3BridgeOp__getOutput(struct soap*, G3BridgeType__JobIDList *jobids, struct G3BridgeOp__getOutputResponse &_param_4)
+{
+	/* XXX */
+	return SOAP_OK;
+}
+
+/**********************************************************************
+ * The SOAP thread handler
+ */
+
+static void soap_service_handler(void *data, void *user_data G_GNUC_UNUSED)
+{
+	struct soap *soap = (struct soap *)data;
+
+	soap_serve(soap);
+	soap_destroy(soap);
+	soap_end(soap);
+	soap_free(soap);
+}
+
+/**********************************************************************
+ * The main program
+ */
+
+static void sigint_handler(int signal __attribute__((__unused__)))
+{
+	finish = true;
+}
 
 int main(int argc, char **argv)
 {
-	int port;
+	int port, ws_threads, dl_threads;
         struct soap soap;
-        pthread_t process_thread;
+	struct sigaction sa;
+
+	Logging::init(cout, LOG_INFO);
 
 	if (argc != 2)
 	{
@@ -34,25 +226,106 @@ int main(int argc, char **argv)
 	g_key_file_load_from_file(global_config, argv[1], G_KEY_FILE_NONE, &error);
 	if (error)
 	{
-		cerr << "Failed to load the config file: " << error->message << endl;
+		LOG(LOG_ERR, "Failed to load the config file: %s", error->message);
+		g_error_free(error);
 	        exit(1);
 	}
-	port = g_key_file_get_integer(global_config, "service", "port", &error);
+
+	char *level = g_key_file_get_string(global_config, GROUP_DEFAULTS, "log-level", NULL);
+	if (level)
+	{
+		Logging::init(cout, level);
+		g_free(level);
+	}
+
+	port = g_key_file_get_integer(global_config, GROUP_WEBSERVICE, "port", &error);
 	if (!port || error)
 	{
-		cerr << "Failed to parse port: " << error->message << endl;
+		LOG(LOG_ERR, "Failed to retrieve the listener port: %s", error->message);
+		g_error_free(error);
 	        exit(1);
 	}
-	if (port < 0)
+	if (port <= 0 || port > 65535)
 	{
-		cerr << "Invalid port number specified, please use unsigned integers" << endl;
+		LOG(LOG_ERR, "Invalid port number (%d) specified", port);
 		exit(-1);
 	}
-	workdir = g_key_file_get_string(global_config, "service", "workdir", &error);
-	if (!workdir || error)
+
+	dl_threads = g_key_file_get_integer(global_config, GROUP_WEBSERVICE, "download-threads", &error);
+	if (!dl_threads || error)
 	{
-		cerr << "Failed to get working directory: " << error->message << endl;
+		LOG(LOG_ERR, "Failed to parse the number of download threads: %s", error->message);
+		g_error_free(error);
 	        exit(1);
+	}
+	if (dl_threads <= 0 || dl_threads > 1000)
+	{
+		LOG(LOG_ERR, "Invalid thread number (%d) specified", dl_threads);
+		exit(-1);
+	}
+
+	ws_threads = g_key_file_get_integer(global_config, GROUP_WEBSERVICE, "service-threads", &error);
+	if (!ws_threads || error)
+	{
+		LOG(LOG_ERR, "Failed to parse the number of service threads: %s", error->message);
+		g_error_free(error);
+	        exit(1);
+	}
+	if (ws_threads <= 0 || ws_threads > 1000)
+	{
+		LOG(LOG_ERR, "Invalid thread number (%d) specified", ws_threads);
+		exit(-1);
+	}
+
+	download_dir = g_key_file_get_string(global_config, GROUP_WEBSERVICE, "download-dir", &error);
+	if (!download_dir || error)
+	{
+		LOG(LOG_ERR, "Failed to get the download directory: %s", error->message);
+		g_error_free(error);
+	        exit(1);
+	}
+
+	partial_dir = g_key_file_get_string(global_config, GROUP_WEBSERVICE, "partial-dir", &error);
+	if (!partial_dir || error)
+	{
+		LOG(LOG_ERR, "Failed to get the partial directory: %s", error->message);
+		g_error_free(error);
+	        exit(1);
+	}
+
+	output_dir = g_key_file_get_string(global_config, GROUP_WEBSERVICE, "output-dir", &error);
+	if (!output_dir || error)
+	{
+		LOG(LOG_ERR, "Failed to get the output base directory: %s", error->message);
+		g_error_free(error);
+	        exit(1);
+	}
+
+	output_url_prefix = g_key_file_get_string(global_config, GROUP_WEBSERVICE, "output-url-prefix", &error);
+	if (!output_url_prefix || error)
+	{
+		LOG(LOG_ERR, "Failed to get the output URL prefix: %s", error->message);
+		g_error_free(error);
+	        exit(1);
+	}
+
+	/* Set up the signal handlers */
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = sigint_handler;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+	sa.sa_handler = SIG_IGN;
+	sigaction(SIGPIPE, &sa, NULL);
+
+	/* Initialize glib's thread system */
+	g_thread_init(NULL);
+
+	dl_pool = g_thread_pool_new(download_handler, NULL, dl_threads, TRUE, NULL);
+	if (!dl_pool)
+	{
+		fprintf(stderr, "Failed to launch download threads\n");
+		exit(1);
 	}
 
 	soap_init(&soap);
@@ -68,7 +341,14 @@ int main(int argc, char **argv)
 		exit(-1);
 	}
 
-        for (;;)
+	soap_pool = g_thread_pool_new(soap_service_handler, NULL, ws_threads, TRUE, NULL);
+	if (!soap_pool)
+	{
+		fprintf(stderr, "Failed to launch the WS threads\n");
+		exit(1);
+	}
+
+        while (!finish)
         {
     		SOAP_SOCKET cs = soap_accept(&soap);
                 if (!cs)
@@ -76,121 +356,14 @@ int main(int argc, char **argv)
                         soap_print_fault(&soap, stderr);
                         exit(-1);
 		}
-		cout << "New connection!" << endl;
                 struct soap *tsoap = soap_copy(&soap);
-		pthread_create(&process_thread, NULL, (void*(*)(void*))process_request, (void*)tsoap);
+		g_thread_pool_push(soap_pool, tsoap, NULL);
 	}
+
 	soap_done(&soap);
+
+	g_thread_pool_free(dl_pool, TRUE, TRUE);
+	g_thread_pool_free(soap_pool, TRUE, TRUE);
+
 	return 0;
-}
-
-
-void *process_request(void *soap)
-{
-        pthread_detach(pthread_self());
-        soap_serve((struct soap*)soap);
-        soap_destroy((struct soap*)soap);
-        soap_end((struct soap*)soap);
-        soap_free((struct soap*)soap);
-        return NULL;
-}
-
-
-int ns__addJob(struct soap *soap, struct ns__Job job, string &jobid)
-{
-        uuid_t id;
-        char sid[37];
-        uuid_generate(id);
-        uuid_unparse(id, sid);
-
-	cout << "Serving ns__addJob..." << endl;
-	cout << " Job alg:  " << job.alg << endl;
-	cout << " Job grid: " << job.grid << endl;
-	cout << " Job args: " << job.args << endl;
-	cout << " Job ins:  " << job.inputs->__size << endl;
-	if (job.outputs)
-		cout << " Job outs: " << job.outputs->__size << endl;
-	for (int i = 0; i < job.inputs->__size; i++)
-		cout << " Job in:   " << job.inputs->__ptr[i] << endl;
-	if (job.outputs)
-		for (int i = 0; i < job.outputs->__size; i++)
-			cout << " Job out:  " << job.outputs->__ptr[i] << endl;
-        Job bJob((const char *)sid, job.alg.c_str(), job.grid.c_str(), job.args.c_str(), Job::INIT);
-
-        vector<string> fsyspaths;
-        struct soap_multipart *attachment;
-        for (attachment = soap->mime.list; attachment; attachment = attachment->next)
-        {
-                if (!(*attachment).size)
-                        continue;
-                const char *localname = (*attachment).description;
-                if (!localname)
-                        continue;
-                string fsyspath = string(workdir) + "/" + string(sid) + "." + localname;
-                fsyspaths.push_back(fsyspath);
-                int ofile = open(fsyspath.c_str(), O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
-                if (-1 == ofile)
-                        continue;
-                if (-1 == write(ofile, (void*)(*attachment).ptr, (*attachment).size))
-                {
-                        close(ofile);
-                        unlink(fsyspath.c_str());
-                        continue;
-                }
-                close(ofile);
-                bJob.addInput(string(localname), fsyspath);
-        }
-
-        bool allinputok = true;
-        for (int i = 0; i < job.inputs->__size && allinputok; i++)
-        {
-                struct stat sstat;
-                string fsyspath = string(workdir) + "/" + string(sid) + "." + string(job.inputs->__ptr[i]);
-                allinputok |= stat(fsyspath.c_str(), &sstat);
-                unlink(fsyspath.c_str());
-        }
-
-	if (job.outputs)
-	        for (int i = 0; i < job.outputs->__size && allinputok; i++)
-    		{
-            		string fsyspath = string(workdir) + "/" + string(job.outputs->__ptr[i]);
-            		bJob.addOutput(job.outputs->__ptr[i], fsyspath);
-    		}
-
-        if (!allinputok)
-                jobid = "UNKNOWN";
-        else
-                jobid = string(sid);
-
-        DBHandler *dbh = DBHandler::get();
-        dbh->addJob(bJob);
-        DBHandler::put(dbh);
-
-	return SOAP_OK;
-}
-
-
-int ns__getStatus(struct soap *soap, string jobid, string &status)
-{
-        DBHandler *dbh = DBHandler::get();
-	Job tJob;
-	dbh->getJob(tJob, jobid);
-	status = string(statToStr(tJob.getStatus()));
-	DBHandler::put(dbh);
-	return SOAP_OK;
-}
-
-
-int ns__cancel(struct soap *soap, string jobid, ns__cancelResponse *out)
-{
-        DBHandler *dbh = DBHandler::get();
-	dbh->deleteJob(jobid);
-	DBHandler::put(dbh);
-	return SOAP_OK;
-}
-
-
-int ns__getOutput(struct soap *soap, string jobid, struct ns__getOutputResponse *out)
-{
-	return SOAP_OK;
 }
