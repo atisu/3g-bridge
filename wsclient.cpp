2b2dbbfc90d9095458153e7ac68d73b1ec412d1f
zfarkas <zfarkas@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-10-09 10:48:35 +0000

    Client tools for the lcg-CE component.
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1865 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- wsclient.cpp
+++ wsclient.cpp
@@ -16,10 +16,14 @@ void parse_args(ns2__Job &job, const char *args);
 void parse_inputs(ns2__Job &job, const char *inputs);
 void parse_outputs(ns2__Job &job, const char *outputs);
 void handle_add(ns2__Job &job, const char *endpoint);
+void handle_status(const char *jobid, const char *endpoint);
+void handle_del(const char *jobid, const char *endpoint);
+void handle_output(const char *jobid, const char *endpoint);
+
 
 int main(int argc, char **argv)
 {
-	char *mode = NULL, *endpoint = NULL;
+	char *mode = NULL, *endpoint = NULL, *jobid = NULL;
 	ns2__Job job;
 
 	while (1)
@@ -34,10 +38,11 @@ int main(int argc, char **argv)
 			{"args", 1, &c, 'a'},
 			{"ins", 1, &c, 'i'},
 			{"outs", 1, &c, 'o'},
+			{"jobid", 1, &c, 'j'},
 			{"endpoint", 1, &c, 'e'},
 			{"help", 0, &c, 'h'}
 		};
-		d = getopt_long(argc, argv, "m:n:g:a:i:o:e:h", long_options, &option_index);
+		d = getopt_long(argc, argv, "m:n:g:a:i:o:j:e:h", long_options, &option_index);
 		if (-1 == d)
 			break;
 		if (d)
@@ -61,6 +66,9 @@ int main(int argc, char **argv)
 			break;
 		case 'o':
 			parse_outputs(job, optarg);
+			break;
+		case 'j':
+			jobid = strdup(optarg);
 			break;
 		case 'e':
 			endpoint = strdup(optarg);
@@ -87,11 +95,27 @@ int main(int argc, char **argv)
 
 	if (!strcmp(mode, "add"))
 		handle_add(job, endpoint);
+	else if (!strcmp(mode, "status"))
+		handle_status(jobid, endpoint);
+	else if (!strcmp(mode, "del"))
+		handle_status(jobid, endpoint);
+	else if (!strcmp(mode, "output"))
+		handle_status(jobid, endpoint);
+	else
+	{
+		cerr << "Error: invalid mode specified: \"" << mode << "\"!" << endl;
+		help_screen(true);
+	}
 
 	return(0);
 }
 
 
+/**
+ * Print usage instructions
+ * @param doshort print how to invoke help when true and exit, print full usage
+ *                otherwise
+ */
 void help_screen(bool doshort)
 {
 	if (doshort)
@@ -117,6 +141,12 @@ void help_screen(bool doshort)
 }
 
 
+/**
+ * Split string.
+ * @param src string to split
+ * @param delims delimiters to use
+ * @param[out] res vector to store splitted values
+ */
 void split_by_delims(const char *src, const char *delims, vector<string> &res)
 {
 	char *tsrc = strdup(src);
@@ -130,12 +160,22 @@ void split_by_delims(const char *src, const char *delims, vector<string> &res)
 }
 
 
+/**
+ * Parse command line arguments
+ * @param[out] job ns2__Job object to fill in
+ * @param args argument string to parse
+ */
 void parse_args(ns2__Job &job, const char *args)
 {
 	split_by_delims(args, ",", job.args);
 }
 
 
+/**
+ * Parse input files
+ * @param[out] job ns2__Job object to fill in
+ * @param inputs inputs string to parse
+ */
 void parse_inputs(ns2__Job &job, const char *inputs)
 {
 	vector<string> ins;
@@ -157,12 +197,22 @@ void parse_inputs(ns2__Job &job, const char *inputs)
 }
 
 
+/**
+ * Parse output files
+ * @param[out] job ns2__Job object to fill in
+ * @param outputs outputs string to parse
+ */
 void parse_outputs(ns2__Job &job, const char *outputs)
 {
 	split_by_delims(outputs, ",", job.outputs);
 }
 
 
+/**
+ * Handle submission
+ * @param job ns2__Job object to send to the Service
+ * @param endpoint Service endpoint to use
+ */
 void handle_add(ns2__Job &job, const char *endpoint)
 {
 	if ("" == job.alg || "" == job.grid)
@@ -173,6 +223,7 @@ void handle_add(ns2__Job &job, const char *endpoint)
 
 	struct ns1__submitResponse IDs;
 	ns2__JobList jList;
+	jList.job.clear();
 	jList.job.push_back(&job);
 
 	struct soap *soap = soap_new();
@@ -183,6 +234,123 @@ void handle_add(ns2__Job &job, const char *endpoint)
 		exit(-1);
 	}
 	cout << "The job ID is: " << IDs.jobids->jobid.at(0) << endl;
+	soap_destroy(soap);
+	soap_end(soap);
+	soap_done(soap);
+}
+
+
+/**
+ * Check if job identifier is not empty. Exits when job identifier is empty
+ * @param val string to check
+ */
+void check_jobid(const char *val)
+{
+	if (val && strlen(val))
+		return;
+		
+	cerr << "Error: job identifier isn't set!" << endl;
+	help_screen(true);
+}
+
+
+/**
+ * Handle status query
+ * @param jobid job identifier we would like to use
+ * @param endpoint Service endpoint to use
+ */
+void handle_status(const char *jobid, const char *endpoint)
+{
+	ns2__JobIDList jList;
+	struct ns1__getStatusResponse resp;
+	struct {
+		ns2__JobStatus st;
+		string str;
+	} statToStr[] = {
+		{ns2__JobStatus__UNKNOWN,  "Unknown" },
+		{ns2__JobStatus__INIT,     "Init"    },
+		{ns2__JobStatus__RUNNING,  "Running" },
+		{ns2__JobStatus__FINISHED, "Finished"},
+		{ns2__JobStatus__ERROR,    "Error"   }
+	};
+
+	check_jobid(jobid);
+	jList.jobid.clear();
+	jList.jobid.push_back(string(jobid));
+
+	struct soap *soap = soap_new();
+	soap_init(soap);
+	if (SOAP_OK != soap_call_ns1__getStatus(soap, endpoint, NULL, &jList, resp))
+	{
+		soap_print_fault(soap, stderr);
+		exit(-1);
+	}
+	ns2__JobStatus st = resp.statuses->status.at(0);
+	soap_destroy(soap);
+	soap_end(soap);
+	soap_done(soap);
+
+	cout << "Status of job \"" << jobid << "\" is: " << statToStr[st].str << endl;
+}
+
+
+/**
+ * Handle job delete
+ * @param jobid job identifier we would like to use
+ * @param endpoint Service endpoint to use
+ */
+void handle_del(const char *jobid, const char *endpoint)
+{
+	ns2__JobIDList jList;
+	struct ns1__delJobResponse resp;
+
+	check_jobid(jobid);
+	jList.jobid.clear();
+	jList.jobid.push_back(string(jobid));
+
+	struct soap *soap = soap_new();
+	soap_init(soap);
+	if (SOAP_OK != soap_call_ns1__delJob(soap, endpoint, NULL, &jList, resp))
+	{
+		soap_print_fault(soap, stderr);
+		exit(-1);
+	}
+	soap_destroy(soap);
+	soap_end(soap);
+	soap_done(soap);
+}
+
+
+/**
+ * Handle job output query
+ * @param jobid job identifier we would like to use
+ * @param endpoint Service endpoint to use
+ */
+void handle_output(const char *jobid, const char *endpoint)
+{
+	ns2__JobIDList jList;
+	struct ns1__getOutputResponse resp;
+
+	check_jobid(jobid);
+	jList.jobid.clear();
+	jList.jobid.push_back(string(jobid));
+
+	struct soap *soap = soap_new();
+	soap_init(soap);
+	if (SOAP_OK != soap_call_ns1__getOutput(soap, endpoint, NULL, &jList, resp))
+	{
+		soap_print_fault(soap, stderr);
+		exit(-1);
+	}
+
+	ns2__JobOutput *jout = resp.outputs->output.at(0);
+	cout << "Output files for job \"" << jout->jobid << "\":" << endl;
+	for (unsigned i = 0; i < jout->output.size(); i++)
+	{
+		ns2__LogicalFile *lf = jout->output.at(i);
+		cout << "    " << lf->logicalName << " - " << lf->URL << endl;
+	}
+
 	soap_destroy(soap);
 	soap_end(soap);
 	soap_done(soap);
