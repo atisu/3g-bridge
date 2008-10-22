056f4cd770315cdcb59a8e17398c0d29be8ae020
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2008-10-22 12:21:22 +0000

    Add some documentation
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/cancergrid/trunk/queue_manager@1923 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- Util.h
+++ Util.h
@@ -10,13 +10,44 @@ extern "C" {
 
 #include <glib.h>
 
+/**
+ * Initialize the logging system.
+ * @param config global configuration
+ * @param section section to use inside the configuration
+ */
 void log_init(GKeyFile *config, const char *section);
+
+/**
+ * Re-open the log file if needed.
+ */
 void log_reopen(void);
 
+/**
+ * Print a log message.
+ * @param lvl the log level
+ * @param fmt format string
+ * @param ... arguments matching the format string
+ */
 void logit(int lvl, const char *fmt, ...) __attribute__((__format__(printf, 2, 3)));
+
+/**
+ * Print a log message.
+ * @param lvl the log level
+ * @param fmt format string
+ * @param ap argument list matching the format string
+ */
 void vlogit(int lvl, const char *fmt, va_list ap);
 
+/**
+ * Create a pid file.
+ * @param config the global configuration
+ * @param section the section to use inside the configuration
+ */
 int pid_file_create(GKeyFile *config, const char *section);
+
+/**
+ * Update the pid file with the actual pid of the program.
+ */
 void pid_file_update(void);
 
 #ifdef __cplusplus
