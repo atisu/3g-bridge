41f3a40f66db9768edace39899080a859af4184f
vis1 <vis1@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2011-05-23 11:34:42 +0000

    Reintegrated the meta-job feature as a built-in feature (non-optional). Added extra indexes to the database for scalability.
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/3g-bridge/branches/metajob@2599 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- db/schema-add-metajob.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE cg_job
-      ADD COLUMN	metajobid	CHAR(36)	NULL,	/* Id referencing a sub-job's parent meta-job. */
-
-      ADD FOREIGN KEY (metajobid) REFERENCES cg_job(id) ON DELETE CASCADE;
-
-CREATE INDEX idx_cg_job_metajobid ON cg_job (metajobid);
