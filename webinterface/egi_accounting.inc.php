c1e07f84567f3bac165a83edaafe8e76a5540dc9
vis1 <vis1@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2013-03-08 09:51:17 +0000

    Implement EGI accounting interface
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/3g-bridge/trunk@2729 a7169a2c-3604-0410-bc95-c702d8d87f7a

---
+++ webinterface/egi_accounting.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('rest_base.inc.php');
+require_once('db.inc.php');
+require_once('auth.inc.php');
+require_once('basic_handlers.inc.php');
+
+/*
+  Handles querying individual jobs for accounting information.
+ */
+class AccinfoHandler extends cg_job_Handler
+{
+	protected function where($ids)
+	{
+		if ($ids == "'*'")
+			return $this->auth_sql_filter(' WHERE ');
+		elseif (strpos($ids, ',') !== FALSE)
+			return "WHERE id in ({$ids})" . $this->auth_sql_filter(' AND ');
+                else
+                        return "WHERE id = {$ids}" . $this->auth_sql_filter(' AND ');
+	}
+
+	protected function handleGet() {
+		$ids = $this->get_selected_ids();
+		$field = $this->get_selected_attrs();
+                $w = $this->where($ids);
+
+		$r = new ResWrapper(
+			DB::q("SELECT {$field} "
+                              . " FROM accounting_info_metajob "
+			      . $w
+                              . " UNION "
+                              . " SELECT {$field} "
+                              . " FROM accounting_info_nonmetajob "
+			      . $w));
+
+		while ($line = mysql_fetch_array($r->res, MYSQL_ASSOC))
+			$this->output_dataitem($line);
+	}
+	protected function allowed() {
+		return "GET";
+	}
+	public static function pathRegex() {
+		return '|^/jobs/(?<id>[^/]+)/accinfo(/(?<attr>[^/]*)/?)?$|';
+	}
+}
+
+?>
