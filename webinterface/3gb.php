c6508e6911c5e6c8f1f6ac86b2a4e011463d6450
vis1 <vis1@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2013-04-24 14:18:51 +0000

    REST i/f: Extract configuration from 3gb.php
    Updates to 3gb.php will not overwrite server configuration.
    
    git-svn-id: https://edges-3g-bridge.svn.sourceforge.net/svnroot/edges-3g-bridge/3g-bridge/trunk@2732 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- webinterface/3gb.php
+++ webinterface/3gb.php
@@ -1,18 +1,6 @@
 <?php
 
-// BASE_URL must point to the REST interface itself.
-// This setting is used for redirection.
-define('BASE_URL', 'https://<project_url>/download/wss');
-// The path of the 3G Bridge condfiguration.
-// You should probably leave this as is, and create a hard link to the
-// file. This way, it should work no matter how Apache is configured.
-define('CONFIG_FILE', '3g-bridge.conf');
-// Command used to query the version of the Bridge used.
-// Make sure it executes the binary being used.
-define('BRIDGE_VER_CMD',
-       '/bin/bash -c /usr/sbin/3g-bridge\ -V');
-// Destination grid for incoming meta-jobs.
-define('METAJOB_GRID', 'Metajob');
+require_once('config.inc.php');
 
 ini_set('display_errors','On');
 error_reporting(E_ALL);
